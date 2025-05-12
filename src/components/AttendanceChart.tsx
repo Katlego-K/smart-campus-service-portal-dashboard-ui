0186d6b58df32b21b61f891c023563d4b3b7b1f9
Katlego Kamasha <kamasha_k@yahoo.com>
2025-05-12 13:15:34 +0200

    Added Events Calendar

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -39,29 +39,32 @@ const data = [
 const AttendanceChart = () => {
     return (
         <div className="br-white rounded-lg p-4 h-full">
-            <div className='className="flex justify-between items-center"'>
-                <h1 className='text-lg font-semibold'>Attendance</h1>
-                <Image src="/moreDark.png" alt='' width={20} height={20}/>
+            <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold">Attendance</h1>
+                <Image src="/moreDark.png" alt="" width={20} height={20}/>
             </div>
             <ResponsiveContainer width="100%" height="90%">
-                <BarChart
-                width={500}
-                height={300}
-                data={data}
-                barSize={20}
-                >
-                <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#ddd' />
-                <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false}/>
+                <BarChart width={500} height={300} data={data} barSize={20}>
+                <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+                <XAxis 
+                dataKey="name" 
+                axisLine={false} 
+                tick={{fill:"#d1d5db"}} 
+                tickLine={false}/>
                 <YAxis axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false}/>
                 <Tooltip
                     contentStyle={{ borderRadius:"10px",borderColor:"lightgray"}}
                 />
-                <Tooltip />
-                <Legend align='left' verticalAlign='top' wrapperStyle={{paddingTop:"20px",paddingBottom:"40px"}}/>
-                <Bar dataKey="present"
-                     fill="#FAE27C"
-                    legendType='circle'
-                    radius={[10,10,0,0]}
+                <Tooltip contentStyle={{borderRadius: "10px", borderColor: "lightgrey"}}/>
+                <Legend 
+                align='left' 
+                verticalAlign='top'
+                 wrapperStyle={{paddingTop:"20px",paddingBottom:"40px"}}/>
+                <Bar 
+                  dataKey="present"
+                  fill="#FAE27C"
+                  legendType='circle'
+                  radius={[10,10,0,0]}
                 />
                 <Bar dataKey="absent"
                      fill="#C3EBFA" 
