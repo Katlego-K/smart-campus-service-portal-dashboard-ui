6df4f5b42e0b72fdd6e98e542e40d43a9693ac44
Katlego Kamasha <kamasha_k@yahoo.com>
2025-05-15 13:57:23 +0200

    added forms for creating new lecturer

--- src/components/forms/LecturerForm.tsx
+++ src/components/forms/LecturerForm.tsx
@@ -3,6 +3,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import InputField from "../InputField";
+
 
 const schema = z.object({
   username: z
@@ -17,10 +19,16 @@ const schema = z.object({
   lastName: z.string().min(1, { message: 'Last name is required!'}),
   phoneNumber: z.string().min(1, { message: 'Phone number is required!'}),
   address: z.string().min(1, { message: 'Addressis required!'}),
+  bloodType: z
+  .string()
+  .min(1, { message: 'Blood Type is required!'})
+  .max(3, { message: 'Blood Type must be at most 3 characters long!'}),
   dateOfBirth: z.date( { message: 'Date of birth is required!'}),
   gender: z.enum(["male","female"], {message: "Gender is required!"}),
   img: z.instanceof(File, {message: "Image is required!"})
 }); 
+
+type Inputs = z.infer<typeof schema>;
  
 const LecturerForm = ({
     type, 
@@ -34,23 +42,108 @@ const LecturerForm = ({
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm({
+    } = useForm<Inputs>({
         resolver: zodResolver(schema),
     });
 
+    const onSubmit = handleSubmit(data =>{
+        console.log(data);
+    });
+
      return(
-        <form className="flex flex-col gap-8">
+        <form className="flex flex-col gap-8" onSubmit={onSubmit}>
             <h1 className="text-xl font-semibold">Create a new lecturer</h1>
-            <span className="text-xs text-gray-gray-400 font-mediumo">Authentication Information</span>
-            <input type="text" 
-                {...register("username")} 
-                className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm"
-            />
-                {errors.username?.message && <p>{errors.username?.message.toString()}</p>}
-            <span className="text-xs text-gray-gray-400 font-mediumo">
+            <span className="text-xs text-gray-400 font-medium">
+                Authentication Information
+            </span>
+            <div className="flex justify-between flex-wrap gap-4">
+                <InputField 
+                    label="Username"
+                    name="username"
+                    defaultValue={data?.username}
+                    register={register}
+                    error={errors.username} 
+                />
+                <InputField 
+                    label="Email"
+                    name="email"
+                    type="email"
+                    defaultValue={data?.email}
+                    register={register}
+                    error={errors.email} 
+                />
+                <InputField 
+                    label="Password"
+                    name="password"
+                    type="password"
+                    defaultValue={data?.password}
+                    register={register}
+                    error={errors.password} 
+                />
+            </div>
+            <span className="text-xs text-gray-400 font-medium">
                 Personal Information
             </span>
-            <button className="bg-blue text-white p-2 rounded-md">{type === "create" ? "Create" : "Update"}</button>
+            <div className="flex justify-between flex-wrap gap-4">
+                <InputField 
+                    label="First Name"
+                    name="firstName"
+                    defaultValue={data?.firstName}
+                    register={register}
+                    error={errors.firstName} 
+                />
+                <InputField 
+                    label="Last Name"
+                    name="lastName"
+                    defaultValue={data?.lastName}
+                    register={register}
+                    error={errors.lastName} 
+                />
+                <InputField 
+                    label="Phone Number"
+                    name="phoneNumber"
+                    defaultValue={data?.phoneNumber}
+                    register={register}
+                    error={errors.phoneNumber} 
+                />
+                <InputField 
+                    label="Address"
+                    name="address"
+                    defaultValue={data?.address}
+                    register={register}
+                    error={errors.address} 
+                />
+                <InputField 
+                    label="Blood Type"
+                    name="bloodType"
+                    defaultValue={data?.bloodType}
+                    register={register}
+                    error={errors.bloodType} 
+                />
+                <InputField 
+                    label="Date of Birth"
+                    name="dateOfBirth"
+                    type="date"
+                    defaultValue={data?.dateOfBirth}
+                    register={register}
+                    error={errors.dateOfBirth} 
+                />
+            </div>
+            <div className="flex flex-col gap-2 w-full md:w-1/4">
+                <label className="text-xs text-gray-500">Gender</label>
+                    <select className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full" {...register("gender")} defaultValue={data?.gender}>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                        {errors.gender?.message && (
+                    <p className="text-xs text-red-400">
+                        {errors.gender.message.toString()}
+                    </p>
+                )}
+            </div>
+            <button className="bg-blue-400 text-white p-2 rounded-md">
+                {type === "create" ? "Create" : "Update"}
+                </button>
         </form>
      )
 }
