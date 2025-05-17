df11d6f73c7de8aeffda5df792588d0925a3a3f5
Katlego Kamasha <kamasha_k@yahoo.com>
2025-05-17 14:24:40 +0200

    added clerk provider

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,6 +6,10 @@ import { useRouter } from "next/navigation";
 const Pagination =({ page, count}: { page: number; count: number}) =>{
 
     const router = useRouter()
+    
+    const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
+
+    const hasNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
 
     const changePage = (newPage:number) =>{
         const params = new URLSearchParams(window.location.search)
@@ -14,7 +18,14 @@ const Pagination =({ page, count}: { page: number; count: number}) =>{
     };
     return(
         <div className="p-4 flex items-center justify-between text-gray-500">
-            <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
+            <button disabled = {!hasPrev}
+                className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+                onClick={() => {
+                    changePage(page - 1)
+                }}
+            >
+                Prev
+            </button>
             <div className="flex items-center gap-2 text-sm">
                 {Array.from({length:Math.ceil(count / ITEM_PER_PAGE)}, 
                 (_, index)=>{
@@ -23,7 +34,9 @@ const Pagination =({ page, count}: { page: number; count: number}) =>{
                         <button key={pageIndex} className={`px-2 rounded-sm ${
                             page == pageIndex ? "bg-lamaSky" :""
                             }`}
-                            onClick={() => {changePage(pageIndex)}}
+                            onClick={() => {
+                                changePage(pageIndex)
+                            }}
                         >
                             {pageIndex}
                         </button>
@@ -31,7 +44,14 @@ const Pagination =({ page, count}: { page: number; count: number}) =>{
                 }
             )}
             </div>
-            <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Next</button>
+            <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+                disabled={!hasNext}
+                onClick={() => {
+                    changePage(page + 1)
+                }}
+            >
+                Next
+            </button>
         </div>
     )
 }
