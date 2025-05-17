b20c803eb8f46ab33ce2c676e181fe7846b2919c
Katlego Kamasha <kamasha_k@yahoo.com>
2025-05-17 15:34:01 +0200

    Added sign in page and routes to spefic user page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 
 function NavBar() {
@@ -21,7 +22,8 @@ function NavBar() {
                     <span className="text-xs leading-3 font-medium">John Doe</span>
                     <span className="text-[10px] text-gray-500 text-right">Admin</span>
                 </div>
-                <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/>
+                {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/> */}
+                <UserButton/>
             </div>
         </div>
 
